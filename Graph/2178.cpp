6b068cd6dafd3f6c281c4102657c648714b6b86e
류수영 <ryuswimming@SwimmingsMacBookPro.local>
2024-01-16 21:50:06 +0900

    2024.01.16

--- Graph/2178.cpp
+++ Graph/2178.cpp
@@ -5,45 +5,33 @@ using namespace std;
 void bfs();
 void check_for_next_queue(int , int );
 
-int N, M, result;
-int _array[1000][1000];
+int N, M, stage;
+int _array[100][100];
 vector<pair<int, int> > current_queue, next_queue;
 
 int main() {
-    cin >> M >> N;
+    cin >> N >> M;
 
+    char temp;
     for (int i = 0; i < N; i++) {
+        getchar();
         for (int j = 0; j < M; j++) {
-            cin >> _array[i][j];
+            if (getchar() == '1') _array[i][j] = 1;
         }
     }
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (_array[i][j] == 1) {
-                current_queue.push_back(make_pair(i, j));
-            }
-        }
-    }
+    current_queue.push_back(make_pair(0, 0));
 
     bfs();
 
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            if (_array[i][j] == 0) {
-                cout << -1 << '\n';
-                return 0;
-            }
-        }
-    }
-
-    cout << result-1 << '\n';
+    cout << _array[N-1][M-1] << '\n';
 }
 
-// Always define what should be stored in a data structure before using it.
 void bfs() {
     pair<int, int> temp;
-    while (!current_queue.empty()) {
+    stage = 1;
+    while (_array[N-1][M-1] == 1) {
+        stage++;
         for (int i = 0; i < current_queue.size(); i++) {
             temp = current_queue[i];
             check_for_next_queue(temp.first-1, temp.second);
@@ -53,13 +41,13 @@ void bfs() {
         }
         current_queue.clear();
         current_queue.swap(next_queue);
-        result++;
     }
 }
 
 void check_for_next_queue(int a, int b) {
+    if (a == 0 && b == 0) return;
     if (a < 0 || a > N-1 || b < 0 || b > M-1) return;
-    if (_array[a][b] != 0) return;
+    if (_array[a][b] != 1) return;
     next_queue.push_back(make_pair(a, b));
-    _array[a][b] = 1;
+    _array[a][b] = stage;
 }
